47477bf70d88c91291094e3df6ba232c0c144dcf
tendeanclaudior <tendeanclaudio1234@gmail.com>
2023-09-28 10:14:40 +0800

    Initial Commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Dio Tendean on 28/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
